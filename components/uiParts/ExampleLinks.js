1a327e66fc4e7935c8a16e071dbcd564ca5f041b
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2021-07-30 21:58:22 +0900

    Topのページの見栄え良くした

--- components/uiParts/ExampleLinks.js
+++ components/uiParts/ExampleLinks.js
@@ -1,13 +1,13 @@
 import Link from 'next/link';
 
 const links = [
-  { href: '/example1', title: 'example1: Hello World' },
-  { href: '/example2', title: 'example2: スタイル適用' },
-  { href: '/example3', title: 'example3: 配列操作' },
-  { href: '/example4', title: 'example4: レイアウト' },
-  { href: '/example5', title: 'example5: カスタムコンポーネント' },
-  { href: '/example6', title: 'example6: onClickとuseState' },
-  { href: '/example7', title: 'example7: fetch' },
+  { href: '/example1', title: 'Hello World' },
+  { href: '/example2', title: 'スタイル適用' },
+  { href: '/example3', title: '配列操作' },
+  { href: '/example4', title: 'レイアウト' },
+  { href: '/example5', title: 'カスタムコンポーネント' },
+  { href: '/example6', title: 'onClickとuseState' },
+  { href: '/example7', title: 'fetch' },
 ];
 
 const ExampleLinks = () =>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,33 @@
+import AppBar from '@material-ui/core/AppBar';
+import Box from '@material-ui/core/Box';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import Container from '@material-ui/core/container';
 import ExampleLinks from '/components/uiParts/ExampleLinks';
 
 const Index = () => (
-  <Container>
-    <ExampleLinks />
-  </Container>
+  <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          News
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+    <Container style={{ marginTop: '10px' }}>
+      <Typography variant="h3">
+        Example
+      </Typography>
+      <ExampleLinks />
+    </Container>
+  </Box>
 );
 
 export default Index;

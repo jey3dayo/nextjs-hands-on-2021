366c2e20c1d85be7110934181d59ca1afeab704d
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2021-09-03 12:45:39 +0900

    Alertモーダル追加

--- pages/example6.js
+++ pages/example6.js
@@ -2,9 +2,15 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/styles';
 import { CustomButton } from '/components/uiParts/CustomButton';
+
+const Alert = props => {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+};
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -23,6 +29,8 @@ const Example = () => {
   // React.useStateを利用し、変数とセッターを用意
   // これをDOMに組み込むとリアルタイムに画面反映される
   const [value, setValue] = React.useState('テスト');
+  const [open, setOpen] = React.useState(false);
+
   const classes = useStyles();
 
   const handleChange = event => {
@@ -31,12 +39,17 @@ const Example = () => {
 
   const onClick = () => {
     setValue('');
+    setOpen(true);
   };
+
+  const handleClose = () => {
+    setOpen(false);
+  }
 
   return (
     <Container>
       <div className={classes.root}>
-        <div className={classes.title}>React.useState</div>
+        <div className={classes.title}>onClickとuseState</div>
         <Box
           component="form"
           noValidate
@@ -47,7 +60,9 @@ const Example = () => {
           <TextField id="outlined-basic" label="テキスト" variant="outlined" value={value} onChange={handleChange} />
 
           <Box component="span" sx={{ padding: '10px' }}>
-            <CustomButton onClick={onClick} variant="outlined">クリア</CustomButton>
+            <CustomButton onClick={onClick} variant="outlined">
+              クリア
+            </CustomButton>
           </Box>
 
           <Box
@@ -68,6 +83,12 @@ const Example = () => {
             />
           </Box>
         </Box>
+
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="success">
+            クリアしました
+          </Alert>
+        </Snackbar>
 
         <div>
           <a href="https://next.material-ui.com/components/text-fields/" target="_blank" rel="noreferrer">

2de583914ccaaab916753aaf5b2205e5769fc402
仲間 凜 <ic181229@edu.okinawa-ct.ac.jp>
2021-09-07 18:27:51 +0900

    APIで躓いている。

--- pages/apitest.js
+++ pages/apitest.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import getConfig from 'next/config';
 
-const Example = ({ gourmet }) => {
+const Example = ({ gourmet ,}) => {
   return (
     <div>
       <h1>gourmet</h1>
@@ -10,12 +10,22 @@ const Example = ({ gourmet }) => {
   );
 };
 
+const shop = ({ gourmet }) => {
+  var shops;
+  var yooso = gourmet.split(',');
+  for (let i = 0; i < yooso.length; i++) {
+    if (yooso.indexOf("name_kana") != 0) {
+      shops.push(yooso[i - 1]);
+    }
+  }
+}
+
 // export async function getStaticProps() {
 export async function getServerSideProps() {
   // 取得テスト
   const { serverRuntimeConfig } = getConfig();
   const { HOTPEPPER_API_KEY } = serverRuntimeConfig;
-  const gourmetUrl = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${HOTPEPPER_API_KEY}&large_area=Z011&format=json`;
+  const gourmetUrl = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${HOTPEPPER_API_KEY}&middle_area=Y964&format=json`;
   const res = await fetch(gourmetUrl);
   const data = await res.json();
 

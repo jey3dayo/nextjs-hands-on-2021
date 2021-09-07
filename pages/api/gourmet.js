ea123985dfbf8108fcf2fbab975d8b4565239425
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2021-09-07 10:53:57 +0900

    サンプル追加

--- pages/api/gourmet.js
+++ pages/api/gourmet.js
@@ -1,10 +1,16 @@
 import getConfig from 'next/config';
 
-export default async (_, res) => {
+const gourmet = async (req, res) => {
+  const {
+    query: { largeArea = 'Z011' },
+  } = req;
+
   const { serverRuntimeConfig } = getConfig();
   const { HOTPEPPER_API_KEY } = serverRuntimeConfig;
-  const gourmetUrl = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${HOTPEPPER_API_KEY}&large_area=Z011&format=json`;
+  const gourmetUrl = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${HOTPEPPER_API_KEY}&large_area=${largeArea}&format=json`;
   const result = await fetch(gourmetUrl);
   const data = await result.json();
   return res.json(data);
-}
+};
+
+export default gourmet;
---
+++ pages/api/hello.js
@@ -0,0 +1,5 @@
+const hello = (_, res) => res.end('hello');
+
+export default hello;
+
+

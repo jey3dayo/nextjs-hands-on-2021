3395f02e04f325abb5a3515de477d6b1278ad452
Joichiroh <joichiroh.inafuku@gmail.com>
2021-09-08 17:36:50 +0900

    ジャンルと価格帯の選択肢追加

--- components/uiParts/CustomAppBar.js
+++ components/uiParts/CustomAppBar.js
@@ -31,10 +31,24 @@ const CustomAppBar = () => (
         </Grid>
         <Grid xs={3}>
           <NativeSelect>
-            <option>すべて</option>
-            <option>レストラン</option>
-            <option>カフェ</option>
-            <option>寿司屋</option>
+            <option >すべて</option>
+            <option>居酒屋</option>
+            <option>ダイニングバー・バル</option>
+            <option>創作料理</option>
+            <option>和食</option>
+            <option>洋食</option>
+            <option>イタリアン・フレンチ</option>
+            <option>中華</option>
+            <option>焼肉・ホルモン</option>
+            <option>韓国料理</option>
+            <option>アジア・エスニック料理</option>
+            <option>各国料理</option>
+            <option>カラオケ・パーティ</option>
+            <option>バー・カクテル</option>
+            <option>ラーメン</option>
+            <option>お好み焼き・もんじゃ</option>
+            <option>カフェ・スイーツ</option>
+            <option>その他グルメ</option>
           </NativeSelect>
         </Grid>
         <Grid>
@@ -43,9 +57,19 @@ const CustomAppBar = () => (
         <Grid xs={3}>
           <NativeSelect>
             <option>すべて</option>
-            <option>~1500円</option>
-            <option>~3000円</option>
-            <option>~5000円</option>
+            <option>~500円</option>
+            <option>501円~1000円</option>
+            <option>1001円~1500円</option>
+            <option>1501円~2000円</option>
+            <option>2001円~3000円</option>
+            <option>3001円~4001円</option>
+            <option>4001円~5000円</option>
+            <option>5001円~7000円</option>
+            <option>7001円~10000円</option>
+            <option>10001円~15000円</option>
+            <option>15001円~20000円</option>
+            <option>20001円~30000円</option>
+            <option>30001円~</option>
           </NativeSelect>
         </Grid>
       </Grid>

c08fc1f02cc3bd37d1635cdc1f15eeb3484a0347
Joichiroh <joichiroh.inafuku@gmail.com>
2021-09-09 14:30:24 +0900

    ジャンルの部分を作成

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Image from 'next/image';
+import { Autocomplete } from '@material-ui/lab';
 
 const Index = () => {
   const classes = useStyles();
@@ -18,15 +19,85 @@ const Index = () => {
       <CustomAppBar />
       <Container>
         <Typography variant="h4">ジャンル</Typography>
-
+        <Grid container spacing={1} className={classes.genre} justifyContent="center">
+          <Grid item xs={2} className={classes.image}>
+            <Image alt="居酒屋" src="/beer.png" width={100} height={100} />
+            <Typography>居酒屋</Typography>
+          </Grid>
+          <Grid item xs={2} className={classes.image}>
+            <Image alt="ダイニングバー・バル" src="/diningbar.png" width={100} height={100} />
+            <Typography>ダイニングバー・バル</Typography>
+          </Grid>
+          <Grid item xs={2} className={classes.image}>
+            <Image alt="創作料理" src="/creativecusine.png" width={100} height={100} />
+            <Typography>創作料理</Typography>
+          </Grid>
+          <Grid item xs={2} className={classes.image}>
+            <Image alt="和食" src="/japanese.png" width={100} height={100} />
+            <Typography>和食</Typography>
+          </Grid>
+          <Grid item xs={2} className={classes.image}>
+            <Image alt="洋食" src="/western.png" width={100} height={100} />
+            <Typography>洋食</Typography>
+          </Grid>
+          <Grid item xs={2} className={classes.image}>
+            <Image alt="イタリアン・フレンチ" src="/italian.png" width={100} height={100} />
+            <Typography>イタリアン・フレンチ</Typography>
+          </Grid>
+          <Grid item xs={2} className={classes.image}>
+            <Image alt="中華" src="/chinese.png" width={100} height={100} />
+            <Typography>中華</Typography>
+          </Grid>
+          <Grid item xs={2} className={classes.image}>
+            <Image alt="焼肉・ホルモン" src="/bbq.png" width={100} height={100} />
+            <Typography>焼肉・ホルモン</Typography>
+          </Grid>
+          <Grid item xs={2} className={classes.image}>
+            <Image alt="韓国料理" src="/korean.png" width={100} height={100} />
+            <Typography>韓国料理</Typography>
+          </Grid>
+          <Grid item xs={2} className={classes.image}>
+            <Image alt="アジア・エスニック料理" src="/ethnic.png" width={100} height={100} />
+            <Typography>アジア・エスニック料理</Typography>
+          </Grid>
+          <Grid item xs={2} className={classes.image}>
+            <Image alt="各国料理" src="/internationalcusine.png" width={100} height={100} />
+            <Typography>各国料理</Typography>
+          </Grid>
+          <Grid item xs={2} className={classes.image}>
+            <Image alt="カラオケ・パーティ" src="/karaoke.png" width={100} height={100} />
+            <Typography>カラオケ・パーティ</Typography>
+          </Grid>
+          <Grid item xs={2} className={classes.image}>
+            <Image alt="バー・カクテル" src="/cocktail.png" width={100} height={100} />
+            <Typography>バー・カクテル</Typography>
+          </Grid>
+          <Grid item xs={2} className={classes.image}>
+            <Image alt="ラーメン" src="/ramen.png" width={100} height={100} />
+            <Typography>ラーメン</Typography>
+          </Grid>
+          <Grid item xs={2} className={classes.image}>
+            <Image alt="お好み焼き・もんじゃ" src="/okonomiyaki.png" width={100} height={100} />
+            <Typography>お好み焼き・もんじゃ</Typography>
+          </Grid>
+          <Grid item xs={2} className={classes.image}>
+            <Image alt="カフェ・スイーツ" src="/cafe.png" width={100} height={100} />
+            <Typography>カフェ・スイーツ</Typography>
+          </Grid>
+          <Grid item xs={2} className={classes.image}>
+            <Image alt="その他グルメ" src="/others.png" width={100} height={100} />
+            <Typography>その他グルメ</Typography>
+          </Grid>
+        </Grid>
         <Typography variant="h4">オススメ</Typography>
         <Card variant="outlined" className={classes.root} borderRadius="borderRadius">
-          <Grid container >
+          <Grid container>
             <Grid item xs={10}>
               <CardContent className={classes.details}>
                 <Typography variant="h5">お店情報</Typography>
                 <Typography variant="body1">
-                  aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaa aaaaa aaaaa aaaaaa aaaaa aaaa a aaaaaaaaaaaaaaaaa aaaaaa aaaa aaaaaaa
+                  aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaa aaaaa aaaaa aaaaaa aaaaa aaaa a
+                  aaaaaaaaaaaaaaaaa aaaaaa aaaa aaaaaaa
                 </Typography>
               </CardContent>
             </Grid>
@@ -53,6 +124,14 @@ const useStyles = makeStyles({
   },
   cover: {
     width: 151,
+  },
+  genre: {
+    display: 'flex',
+    height: 250,
+    overflow: 'auto',
+  },
+  image: {
+    textAlign: 'center',
   },
 });
 

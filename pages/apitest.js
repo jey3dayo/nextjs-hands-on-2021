7b499e19d2dae87d5c8f5dfcca036e723d7917f8
仲間 凜 <ic181229@edu.okinawa-ct.ac.jp>
2021-09-09 11:35:26 +0900

    SPUに対応した

--- pages/apitest.js
+++ pages/apitest.js
@@ -2,14 +2,49 @@ import React from 'react';
 import getConfig from 'next/config';
 import Head from 'next/head';
 import Link from 'next/link';
+import { Box } from '@material-ui/core';
+import { CustomButton } from '/components/uiParts/CustomButton';
+
+const fetchApi = async (area,budget) => {
+  const query = new URLSearchParams();
+  const { publicRuntimeConfig } = getConfig();
+  const { VERCEL_URL } = publicRuntimeConfig;
+
+  const areaMode = ['small_area', 'middle_area', 'large_area'];
+  const areaCode = ['X119', 'Y964'];
+  const budgetList = ['&budget=B010,B011', '&budget=B001,B002', '&budget=B003,B008', ''];
+  query.set('area', areaCode[area]);
+  query.append('budget', budgetList[budget]);
+
+  const apiUri = `http://${VERCEL_URL}api/getJson?${query.toString()}`;
+  const res = await fetch(apiUri);
+  const data = await res.json();
+ if (!res.ok) {
+   const message = data?.message ?? result;
+   throw new Error(message);
+ }
+
+ return data;
+
+};
 
 const Example = ({ gourmet }) => {
+  const [budget, setBudget] = React.useState(3);
+  const [value, setValue] = React.useState(gourmet ?? '');
+  const setLow = async () => {
+    setBudget(0)
+    const res = await fetchApi(0, budget);
+    setValue(res);
+  };
   return (
     <div>
       <h1>gourmet</h1>
+      <div>
+        <CustomButton onClick={setLow}>0~1500</CustomButton>
+      </div>
 
       <ul>
-        {gourmet.results.shop.map((item, index) => {
+        {value.results.shop.map((item, index) => {
           return (
             <li key={index}>
               <Link href={item.urls.pc}>
@@ -34,31 +69,10 @@ const Example = ({ gourmet }) => {
 // export async function getStaticProps() {
 export async function getServerSideProps() {
   // 取得テスト
-  const query = new URLSearchParams();
-
-  const { serverRuntimeConfig } = getConfig();
-  const { HOTPEPPER_API_KEY } = serverRuntimeConfig;
-
-  const { publicRuntimeConfig } = getConfig();
-  const { VERCEL_URL } = publicRuntimeConfig;
-
-  const areaMode = ['small_area', 'middle_area', 'large_area'];
-  const areaCode = ['X119', 'Y964'];
-  const budgetList = {
-    low: '&budget=B010,B011',
-    middle: '&budget=B001,B002',
-    high: '&budget=B003,B008',
-    all: '',
-  };
-  query.set('area', areaCode[0]);
-  query.append('budget', budgetList.low);
-  const apiUrl = `http://${VERCEL_URL}api/getJson?${query.toString()}`;
-  const res = await fetch(apiUrl);
-  const data = await res.json();
-
+  const res = await fetchApi(0,3);
   return {
     props: {
-      gourmet: data,
+      gourmet: res,
     },
   };
 }
